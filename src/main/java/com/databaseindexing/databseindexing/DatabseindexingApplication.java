075ab6f0245487e5e3748172a689781de29a5939
Abdullah <abdullahit18040@gmail.com>
2023-10-25 11:47:19 +0600

    database indexing

---
+++ src/main/java/com/databaseindexing/databseindexing/DatabseindexingApplication.java
@@ -0,0 +1,15 @@
+package com.databaseindexing.databseindexing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabseindexingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabseindexingApplication.class, args);
+    }
+
+
+
+}
---
+++ src/test/java/com/databaseindexing/databseindexing/DatabseindexingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.databaseindexing.databseindexing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabseindexingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
